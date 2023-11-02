21d753e645082593379d9877863388d0d10388e5
Alexis Ibarra <50971046+axisSN01@users.noreply.github.com>
2023-11-02 15:31:43 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+var iframe = document.querySelector('#iframe'); 
+
+$(document).ready(function(e) { 
+  $('iframe').attr('src','https://app.powerbi.com/view?r=eyJrIjoiM2I5MzI5NjctYzY3OC00YTdhLThkNDMtZmIzYzljZWQzOTljIiwidCI6ImVhYjIyMDQ5LTY0YWUtNDcwOC04YmFlLWRhOTA0NTkwZThkZSJ9'); 
+}); 
