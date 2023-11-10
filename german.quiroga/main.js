1a195339eec2783a762e383f0df34429df2c2e3d
axisSN01 <axs.ibarra@gmail.com>
2023-11-09 22:11:30 -0300

    first commit

---
+++ german.quiroga/main.js
@@ -0,0 +1,6 @@
+var iframe = document.querySelector('#iframe'); 
+
+$(document).ready(function(e) { 
+  $('iframe').attr('src','https://app.powerbi.com/view?r=eyJrIjoiM2I5MzI5NjctYzY3OC00YTdhLThkNDMtZmIzYzljZWQzOTljIiwidCI6ImVhYjIyMDQ5LTY0YWUtNDcwOC04YmFlLWRhOTA0NTkwZThkZSJ9'); 
+  //$('iframe').attr('src','https://app.powerbi.com/reportEmbed?reportId=168ee78e-01ed-4eab-a3a9-6d7dfeca710f&autoAuth=true&ctid=eab22049-64ae-4708-8bae-da904590e8de');
+}); 
