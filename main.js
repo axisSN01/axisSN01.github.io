f967a10b4e5d9b0558d027fd680016f11e3dd5ad
Alexis Ibarra <50971046+axisSN01@users.noreply.github.com>
2023-12-14 12:40:46 -0300

    ded

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var iframe = document.querySelector('.iframe-container iframe'); 
 
 $(document).ready(function(e) {
-    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    var isMobile = "/iPhone|iPad|iPod|Android/i".test(navigator.userAgent);
 
     // var mobileLink = 'https://app.powerbi.com/view?r=eyJrIjoiYmNiZDViOTUtMzRhZC00ZTNlLTgwNjMtY2QyYjZmMDI5YmVhIiwidCI6ImVhYjIyMDQ5LTY0YWUtNDcwOC04YmFlLWRhOTA0NTkwZThkZSJ9&pageName=ReportSection5fc548ad805310641ada';
-  var mobileLink = "https://www.google.com/"  
+  var mobileLink = 'https://www.google.com/';
   var desktopLink = 'https://app.powerbi.com/view?r=eyJrIjoiYmNiZDViOTUtMzRhZC00ZTNlLTgwNjMtY2QyYjZmMDI5YmVhIiwidCI6ImVhYjIyMDQ5LTY0YWUtNDcwOC04YmFlLWRhOTA0NTkwZThkZSJ9&pageName=ReportSection5fc548ad805310641ada';
 
     if (isMobile) {
@@ -13,4 +13,4 @@ $(document).ready(function(e) {
         iframe.src = desktopLink;
     }
 }
-
+)
